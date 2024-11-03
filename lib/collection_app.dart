ff05af44743f118f301f68786ef270b3bbc73595
yumayuma708 <yumaskywalker708@gmail.com>
2024-11-03 19:55:14 +0900

    全てのフォントの種類を定義

--- lib/collection_app.dart
+++ lib/collection_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:mr_collection/home_screen/home_screen.dart';
 
 class CollectionApp extends StatelessWidget {
@@ -7,12 +8,14 @@ class CollectionApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '集金くん',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        brightness: Brightness.light,
+        textTheme: GoogleFonts.montserratTextTheme(),
       ),
-      home: const HomeScreen(title: 'Flutter Demo Home Page'),
+      home: const HomeScreen(
+        title: '集金くん',
+      ),
     );
   }
 }
