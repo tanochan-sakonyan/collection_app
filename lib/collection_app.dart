e298f85558d7c2f027b9334dd4c04b6f17de89d1
yumayuma708 <yumaskywalker708@gmail.com>
2024-11-07 23:17:35 +0900

    タイポグラフィーを定義

--- lib/collection_app.dart
+++ lib/collection_app.dart
@@ -7,11 +7,21 @@ class CollectionApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final baseTextTheme = GoogleFonts.montserratTextTheme();
+
+    final customTextTheme = baseTextTheme.copyWith(
+      bodyLarge: baseTextTheme.bodyLarge?.copyWith(fontSize: 16.0),
+      bodyMedium: baseTextTheme.bodyMedium?.copyWith(fontSize: 14.0),
+      bodySmall: baseTextTheme.bodySmall?.copyWith(fontSize: 12.0),
+      labelLarge: baseTextTheme.labelLarge?.copyWith(fontSize: 11.0),
+      labelSmall: baseTextTheme.labelSmall?.copyWith(fontSize: 10.0),
+    );
+
     return MaterialApp(
       title: '集金くん',
       theme: ThemeData(
         brightness: Brightness.light,
-        textTheme: GoogleFonts.montserratTextTheme(),
+        textTheme: customTextTheme,
       ),
       home: const HomeScreen(
         title: '集金くん',
