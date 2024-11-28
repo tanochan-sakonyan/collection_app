68be0e8495d0dd9ac623e37b8480732475274d68
yumayuma708 <yumaskywalker708@gmail.com>
2024-11-28 21:03:30 +0900

    ログアウトポップアップのUIを修正

--- lib/components/dialog/logout_dialog.dart
+++ lib/components/dialog/logout_dialog.dart
@@ -12,79 +12,68 @@ class LogoutDialog extends StatelessWidget {
     return Dialog(
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(20),
-        side: const BorderSide(color: Colors.black, width: 1),
       ),
       child: Container(
-        width: 296,
-        height: 200,
+        width: 320,
+        height: 179,
         decoration: BoxDecoration(
-          color: const Color(0xFFF2F2F2),
-          borderRadius: BorderRadius.circular(20),
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(23),
         ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Padding(
-              padding: EdgeInsets.only(top: 44.0, bottom: 50.0),
-              child: Column(
-                children: [
-                  Text(
-                    'ログアウトしますか？',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                      color: Colors.black,
-                    ),
-                  ),
-                ],
+            Text(
+              'ログアウトしますか？',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+                color: Colors.black,
               ),
             ),
-            const Divider(height: 1, color: Colors.black),
-            Stack(
+            SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Container(
-                    height: 80,
-                    alignment: Alignment.center,
-                    child:
-                        const VerticalDivider(width: 1, color: Colors.black)),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    const Spacer(),
-                    GestureDetector(
-                      onTap: () => _signOut(context),
-                      child: Container(
-                        height: 80,
-                        alignment: Alignment.center,
-                        child: const Text(
-                          'はい',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
-                          ),
-                        ),
+                const Spacer(flex: 2),
+                SizedBox(
+                  height: 36,
+                  width: 107,
+                  child: ElevatedButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFFD7D7D7),
+                      elevation: 2,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
                       ),
                     ),
-                    const Spacer(flex: 2),
-                    GestureDetector(
-                      onTap: () => Navigator.of(context).pop(),
-                      child: Container(
-                        height: 80,
-                        alignment: Alignment.center,
-                        child: const Text(
-                          'いいえ',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ),
+                    child: Text(
+                      'いいえ',
+                      style: Theme.of(context).textTheme.bodyMedium,
                     ),
-                    const Spacer(),
-                  ],
+                  ),
                 ),
+                const Spacer(),
+                SizedBox(
+                  height: 36,
+                  width: 107,
+                  child: ElevatedButton(
+                    onPressed: () => _signOut(context),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFFF2F2F2),
+                      elevation: 2,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    child: Text(
+                      'はい',
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                  ),
+                ),
+                const Spacer(flex: 2),
               ],
             ),
           ],
