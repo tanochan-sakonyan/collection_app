b961560ef18be8d1ed2643fe10941ed59db53dfb
yumayuma708 <yumaskywalker708@gmail.com>
2024-12-10 18:39:16 +0900

    FABの位置を少し変更

--- lib/ui/components/member_list.dart
+++ lib/ui/components/member_list.dart
@@ -303,7 +303,7 @@ class MemberList extends ConsumerWidget {
           ),
           Positioned(
             right: 4,
-            bottom: 163,
+            bottom: 100,
             child: SizedBox(
               height: 60,
               width: 60,
