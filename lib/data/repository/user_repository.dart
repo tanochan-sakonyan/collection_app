62bdba0c18c3205e9147b5159ecf0a334171f9d0
yumayuma708 <yumaskywalker708@gmail.com>
2025-02-03 23:23:44 +0900

    baseUrlとエンドポイントを変更，BEとの通信には成功

--- lib/data/repository/user_repository.dart
+++ lib/data/repository/user_repository.dart
@@ -11,12 +11,14 @@ class UserRepository {
   // アクセストークンを送って、ユーザー情報を取得する
   Future<User?> registerUser(String accessToken) async {
     debugPrint('fetchUser関数が呼ばれました。');
-    final url = Uri.parse('$baseUrl/users');
+    debugPrint('アクセストークン: $accessToken');
+    final url = Uri.parse('$baseUrl/users/test');
 
     final response = await http.post(
       url,
       headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({'line_token': accessToken}),
+      body: jsonEncode({}),
+      // body: jsonEncode({'line_token': accessToken}),
     );
 
     debugPrint('Response status: ${response.statusCode}');
@@ -52,7 +54,7 @@ class UserRepository {
 
     final response = await http.get(
       url,
-      headers: {'Content-Type': 'application/json'},
+      // headers: {'Content-Type': 'application/json'},
     );
 
     debugPrint('Response status: ${response.statusCode}');
