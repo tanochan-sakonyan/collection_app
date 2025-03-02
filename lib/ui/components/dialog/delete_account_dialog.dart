5630f5e80427d72b775fd46a2d7c93312ed669bd
yumayuma708 <yumaskywalker708@gmail.com>
2025-03-02 16:09:10 +0900

    アカウントを削除する確認のポップアップを作成、表示

---
+++ lib/ui/components/dialog/delete_account_dialog.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+
+class DeleteAccountDialog extends StatefulWidget {
+  const DeleteAccountDialog({super.key});
+
+  @override
+  State<DeleteAccountDialog> createState() => _DeleteAccountDialogState();
+}
+
+class _DeleteAccountDialogState extends State<DeleteAccountDialog> {
+  bool _checked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(23.0),
+      ),
+      child: SizedBox(
+        width: 320,
+        height: 350,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'アカウントの削除',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(height: 12),
+              const Text(
+                '※ 注意 ※',
+                style: TextStyle(
+                  color: Colors.red,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(height: 8),
+              const Text(
+                'アカウントを削除した場合、\nすべてのデータが初期化されます。\n'
+                'データの復旧を行うことは\nできません。',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14,
+                ),
+              ),
+              const SizedBox(height: 8),
+              const Text(
+                '本当によろしいですか？',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14,
+                ),
+              ),
+              const SizedBox(height: 8),
+              Row(
+                children: [
+                  Checkbox(
+                    value: _checked,
+                    fillColor: WidgetStateProperty.resolveWith((states) {
+                      if (states.contains(WidgetState.selected)) {
+                        return const Color(0xFF000000);
+                      }
+                      return const Color(0xFFB8B7B7);
+                    }),
+                    side: const BorderSide(width: 0, color: Colors.transparent),
+                    onChanged: (bool? value) {
+                      setState(() {
+                        _checked = value ?? false;
+                      });
+                    },
+                  ),
+                  const Expanded(
+                    child: Text(
+                      '上記を確認し、削除を希望する',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 12,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 36,
+                    width: 107,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop(false);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: _checked
+                            ? const Color(0xFFD7D7D7)
+                            : const Color(0xFF8C8C8C),
+                      ),
+                      child: Text(
+                        'いいえ',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 28),
+                  SizedBox(
+                    height: 36,
+                    width: 107,
+                    child: ElevatedButton(
+                      onPressed: _checked
+                          ? () {
+                              Navigator.of(context).pop(true);
+                            }
+                          : null, //TODO 削除機能を実装 //TODO 削除が完了しましたのポップアップに遷移
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: _checked
+                            ? const Color(0xFFF2F2F2)
+                            : const Color(0xFF8C8C8C),
+                      ),
+                      child: Text(
+                        'はい',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
