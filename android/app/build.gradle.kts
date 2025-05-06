ea739a7b5c8771467448e738bb158884b52682f0
yumayuma708 <yumaskywalker708@gmail.com>
2025-05-06 19:34:27 +0900

    アイコンをAndroidにも設定

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
+        versionCode = 4
         versionName = "first relrease"
     }
 
