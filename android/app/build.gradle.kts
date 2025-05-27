da9a68f9caece8a2bef552c76e8c6c6d6372bfdd
oiM-o <sugar_sugar2000@icloud.com>
2025-05-28 07:48:38 +0900

    SplashScreenAPIの動作が担保されているSDKレベルの設定

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,16 +16,16 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "app.web.mr_collection"
-    compileSdk = flutter.compileSdkVersion
+    compileSdkVersion 35
     ndkVersion = "27.0.12077973"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
@@ -34,7 +34,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
-        targetSdk = 34
+        targetSdkVersion 35
         versionCode = 5
         versionName = "first relrease"
     }
@@ -61,4 +61,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    implementation "androidx.core:core-splashscreen:1.0.1"
 }
