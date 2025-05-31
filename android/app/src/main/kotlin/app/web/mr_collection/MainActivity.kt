3e3079c78c3f29ff05ef4d43efc4f2f7d1755632
oiM-o <sugar_sugar2000@icloud.com>
2025-05-31 16:43:19 +0900

    Android12以降以前でも同様にスプラッシュが出るように

--- android/app/src/main/kotlin/app/web/mr_collection/MainActivity.kt
+++ android/app/src/main/kotlin/app/web/mr_collection/MainActivity.kt
@@ -1,5 +1,11 @@
 package app.web.mr_collection
 
 import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
