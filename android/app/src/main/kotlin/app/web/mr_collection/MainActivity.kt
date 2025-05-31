01075bdbd1c3abcd8223a5e9325d1b9c82d3ba48
oiM-o <sugar_sugar2000@icloud.com>
2025-05-31 17:09:39 +0900

    不要なファイルの削除

--- android/app/src/main/kotlin/app/web/mr_collection/MainActivity.kt
+++ android/app/src/main/kotlin/app/web/mr_collection/MainActivity.kt
@@ -4,8 +4,4 @@ import io.flutter.embedding.android.FlutterActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
-class MainActivity : FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-}
+class MainActivity : FlutterActivity()
