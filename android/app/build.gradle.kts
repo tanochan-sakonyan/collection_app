4b6b278d60fd4f8d1828c005433b1a1deb2e9f95
yumayuma708 <yumaskywalker708@gmail.com>
2025-06-08 15:24:51 +0900

    build gradleを削除

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,7 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "app.web.mr_collection"
-    compileSdkVersion 35
+    compileSdk = 35
     ndkVersion = "27.0.12077973"
 
     compileOptions {
@@ -34,7 +34,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
-        targetSdkVersion 35
+        targetSdk = 35
         versionCode = 5
         versionName = "first relrease"
     }
