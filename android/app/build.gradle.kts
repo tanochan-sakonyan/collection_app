7422445a58e06b06a707c46208aaaa54f6e13c19
oiM-o <sugar_sugar2000@icloud.com>
2025-07-03 07:38:56 +0900

    PR時における修正点

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,11 +39,20 @@ android {
         versionName = "first relrease"
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = false
             isShrinkResources = false
         }
