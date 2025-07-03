fbfdcbe5b10d6e500dfc27694fec7799c9b93f01
yumayuma708 <yumaskywalker708@gmail.com>
2025-07-03 16:34:27 +0900

    グループ取得でもグルグル

--- lib/ui/components/dialog/event/add_event_dialog.dart
+++ lib/ui/components/dialog/event/add_event_dialog.dart
@@ -120,9 +120,17 @@ class AddEventDialogState extends ConsumerState<AddEventDialog> {
     final userId = ref.read(userProvider)?.userId;
     if (userId == null) return;
 
-    final lineGroups =
-        await ref.read(userProvider.notifier).getLineGroups(userId);
     debugPrint('LINE取得APIを実行しました。');
+    ref.read(loadingProvider.notifier).state = true;
+
+    List<LineGroup> lineGroups = [];
+    try {
+      lineGroups = await ref.read(userProvider.notifier).getLineGroups(userId);
+    } catch (e) {
+      debugPrint('LINE グループ取得失敗: $e');
+    } finally {
+      ref.read(loadingProvider.notifier).state = false;
+    }
     debugPrint('取得したLINEグループ : $lineGroups');
 
     if (lineGroups.isEmpty) {
@@ -149,212 +157,215 @@ class AddEventDialogState extends ConsumerState<AddEventDialog> {
   Widget build(BuildContext context) {
     return CircleIndicator(
       child: Dialog(
-      backgroundColor: Colors.white,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.only(top: 8, left: 24, right: 24),
-        child: Container(
-          width: 320,
-          height: 330,
-          color: Colors.white,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Center(
-                child: Text(
-                  S.of(context)?.addEvent ?? "Add Event",
-                  style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.w700,
-                      ),
-                ),
-              ),
-              const SizedBox(height: 20),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "Event",
-                  style: Theme.of(context).textTheme.bodySmall?.copyWith(
-                        fontSize: 10.0,
-                        fontWeight: FontWeight.w700,
-                      ),
-                ),
-              ),
-              Container(
-                width: 272,
-                height: 48,
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(color: const Color(0xFFE8E8E8)),
-                ),
-                child: TextField(
-                  controller: _controller,
-                  // textAlignVertical: TextAlignVertical.center,
-                  decoration: const InputDecoration(
-                    border: InputBorder.none,
+        backgroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 8, left: 24, right: 24),
+          child: Container(
+            width: 320,
+            height: 330,
+            color: Colors.white,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Center(
+                  child: Text(
+                    S.of(context)?.addEvent ?? "Add Event",
+                    style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w700,
+                        ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 4),
-              Container(
-                height: 20,
-                width: double.infinity,
-                color: Colors.white,
-                alignment: Alignment.centerRight,
-                child: _errorMessage != null
-                    ? Text(
-                        _errorMessage!,
-                        style: Theme.of(context).textTheme.labelSmall?.copyWith(
-                              fontSize: 10,
-                              fontWeight: FontWeight.w300,
-                              color: Colors.red,
-                            ),
-                      )
-                    : null,
-              ),
-              const SizedBox(height: 4),
-              // Options Section
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Column(
-                  children: [
-                    SizedBox(
-                      width: 272,
-                      height: 48,
-                      child: ListTile(
-                        contentPadding: EdgeInsets.zero,
-                        title: Text(
-                          S.of(context)?.transferMembers ?? "Transfer Members",
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyMedium
-                              ?.copyWith(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.black),
+                const SizedBox(height: 20),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Event",
+                    style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                          fontSize: 10.0,
+                          fontWeight: FontWeight.w700,
                         ),
-                        trailing: SizedBox(
-                          width: 112,
-                          height: 28,
-                          child: ElevatedButton(
-                            onPressed: _isButtonEnabled ? _choiceEvent : null,
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: _isTransferMode
-                                  ? const Color(0xFF76DCC6)
-                                  : const Color(0xFFECECEC),
-                              elevation: 2,
-                              shape: const StadiumBorder(),
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 4, vertical: 0),
-                            ),
-                            child: Text(
-                              _selectedEvent?.eventName ??
-                                  S.of(context)?.selectEvent ??
-                                  "Select an Event",
-                              maxLines: 1,
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyMedium
-                                  ?.copyWith(
+                  ),
+                ),
+                Container(
+                  width: 272,
+                  height: 48,
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: const Color(0xFFE8E8E8)),
+                  ),
+                  child: TextField(
+                    controller: _controller,
+                    // textAlignVertical: TextAlignVertical.center,
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 4),
+                Container(
+                  height: 20,
+                  width: double.infinity,
+                  color: Colors.white,
+                  alignment: Alignment.centerRight,
+                  child: _errorMessage != null
+                      ? Text(
+                          _errorMessage!,
+                          style:
+                              Theme.of(context).textTheme.labelSmall?.copyWith(
                                     fontSize: 10,
-                                    fontWeight: FontWeight.w500,
-                                    color: _isTransferMode
-                                        ? Colors.white
-                                        : Colors.black,
+                                    fontWeight: FontWeight.w300,
+                                    color: Colors.red,
                                   ),
+                        )
+                      : null,
+                ),
+                const SizedBox(height: 4),
+                // Options Section
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        width: 272,
+                        height: 48,
+                        child: ListTile(
+                          contentPadding: EdgeInsets.zero,
+                          title: Text(
+                            S.of(context)?.transferMembers ??
+                                "Transfer Members",
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyMedium
+                                ?.copyWith(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.black),
+                          ),
+                          trailing: SizedBox(
+                            width: 112,
+                            height: 28,
+                            child: ElevatedButton(
+                              onPressed: _isButtonEnabled ? _choiceEvent : null,
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: _isTransferMode
+                                    ? const Color(0xFF76DCC6)
+                                    : const Color(0xFFECECEC),
+                                elevation: 2,
+                                shape: const StadiumBorder(),
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 4, vertical: 0),
+                              ),
+                              child: Text(
+                                _selectedEvent?.eventName ??
+                                    S.of(context)?.selectEvent ??
+                                    "Select an Event",
+                                maxLines: 1,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyMedium
+                                    ?.copyWith(
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.w500,
+                                      color: _isTransferMode
+                                          ? Colors.white
+                                          : Colors.black,
+                                    ),
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      width: 272,
-                      height: 48,
-                      child: ListTile(
-                        contentPadding: EdgeInsets.zero,
-                        dense: true,
-                        title: Text(
-                          S.of(context)?.addFromLine ?? "Add From LINE Group",
-                          style: Theme.of(context)
-                              .textTheme
-                              .bodyMedium
-                              ?.copyWith(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.black),
-                        ),
-                        trailing: lineGroup != null
-                            ? SizedBox(
-                                width: 112,
-                                height: 28,
-                                child: ElevatedButton(
-                                  onPressed: null,
-                                  style: ElevatedButton.styleFrom(
-                                    backgroundColor: const Color(0xFF06C755),
-                                    elevation: 2,
-                                    shape: const StadiumBorder(),
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 4, vertical: 0),
+                      SizedBox(
+                        width: 272,
+                        height: 48,
+                        child: ListTile(
+                          contentPadding: EdgeInsets.zero,
+                          dense: true,
+                          title: Text(
+                            S.of(context)?.addFromLine ?? "Add From LINE Group",
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyMedium
+                                ?.copyWith(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.black),
+                          ),
+                          trailing: lineGroup != null
+                              ? SizedBox(
+                                  width: 112,
+                                  height: 28,
+                                  child: ElevatedButton(
+                                    onPressed: null,
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: const Color(0xFF06C755),
+                                      elevation: 2,
+                                      shape: const StadiumBorder(),
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 4, vertical: 0),
+                                    ),
+                                    child: Text(
+                                      lineGroup!.groupName,
+                                      maxLines: 1,
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .bodyMedium
+                                          ?.copyWith(
+                                            fontSize: 10,
+                                            fontWeight: FontWeight.w500,
+                                            color: _isTransferMode
+                                                ? Colors.white
+                                                : Colors.black,
+                                          ),
+                                    ),
+                                  ))
+                              : IconButton(
+                                  icon: SvgPicture.asset(
+                                    'assets/icons/line.svg',
+                                    width: 32,
+                                    height: 32,
                                   ),
-                                  child: Text(
-                                    lineGroup!.groupName,
-                                    maxLines: 1,
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .bodyMedium
-                                        ?.copyWith(
-                                          fontSize: 10,
-                                          fontWeight: FontWeight.w500,
-                                          color: _isTransferMode
-                                              ? Colors.white
-                                              : Colors.black,
-                                        ),
-                                  ),
-                                ))
-                            : IconButton(
-                                icon: SvgPicture.asset(
-                                  'assets/icons/line.svg',
-                                  width: 32,
-                                  height: 32,
+                                  onPressed: (_isButtonEnabled &&
+                                          !ref.watch(loadingProvider))
+                                      ? () => _selectLineGroup()
+                                      : null,
                                 ),
-                                onPressed: _isButtonEnabled
-                                    ? () => _selectLineGroup()
-                                    : null,
-                              ),
+                        ),
                       ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 24),
+                SizedBox(
+                  width: 272,
+                  height: 40,
+                  child: ElevatedButton(
+                    onPressed: _isButtonEnabled ? () => _createEvent() : null,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFFF2F2F2),
+                      elevation: 2,
+                      shape: const StadiumBorder(),
                     ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 24),
-              SizedBox(
-                width: 272,
-                height: 40,
-                child: ElevatedButton(
-                  onPressed: _isButtonEnabled ? () => _createEvent() : null,
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xFFF2F2F2),
-                    elevation: 2,
-                    shape: const StadiumBorder(),
-                  ),
-                  child: Text(
-                    S.of(context)?.confirm ?? "Confirm",
-                    style: GoogleFonts.notoSansJp(
-                        color: Colors.black,
-                        fontSize: 14.0,
-                        fontWeight: FontWeight.w400),
+                    child: Text(
+                      S.of(context)?.confirm ?? "Confirm",
+                      style: GoogleFonts.notoSansJp(
+                          color: Colors.black,
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.w400),
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 20),
+                const SizedBox(height: 20),
               ],
             ),
           ),
