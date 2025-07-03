035f613eb123a973d142d64838a920cc95abc2c4
yumayuma708 <yumaskywalker708@gmail.com>
2025-07-03 18:34:27 +0900

    不要なコメントアウトを削除

--- lib/ads/ad_helper.dart
+++ lib/ads/ad_helper.dart
@@ -1,20 +1,20 @@
 import 'dart:io';
 
-// テスト用のIDを出すヘルパークラス
+// テスト用のIDを返すメソッド
 class AdHelper {
   static String bannerTestId() {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/6300978111'; // Android バナー
+      return 'ca-app-pub-3940256099942544/6300978111'; // Android バナー広告ID
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/2934735716'; // iOS バナー
+      return 'ca-app-pub-3940256099942544/2934735716'; // iOS バナー広告ID
     }
     throw UnsupportedError('Unsupported platform');
   }
 
-  // 参考：本番用 ID を返すメソッドを足しておくと切替えがラク
+  // 本番用 ID を返すメソッド
   static String bannerProdId() {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-xxxxxxxxxxxxxxxx/aaaaaaaaaa'; // 後々設定
+      return 'ca-app-pub-xxxxxxxxxxxxxxxx/aaaaaaaaaa'; // TODO: 後々設定
     } else if (Platform.isIOS) {
       return 'ca-app-pub-8038336425425966/9529142673';
     }
