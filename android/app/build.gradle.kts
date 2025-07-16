11dae73326dcda50b7a3696574b219cbbe7ed649
yumayuma708 <yumaskywalker708@gmail.com>
2025-07-16 09:42:35 +0900

    バージョン番号を変更

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
         targetSdk = 35
-        versionCode = 10
+        versionCode = 11
         versionName = "first relrease"
     }
 
