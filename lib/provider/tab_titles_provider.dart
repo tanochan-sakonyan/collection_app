86b15d9ce3ea9e0633a155d2d9097b0b48fc8404
oiM-o <sugar_sugar2000@icloud.com>
2025-07-19 23:07:09 +0900

    全員支払い済みのイベントを一番右にソート

--- lib/provider/tab_titles_provider.dart
+++ lib/provider/tab_titles_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mr_collection/provider/user_provider.dart';
+import 'package:mr_collection/data/model/payment_status.dart';
 
 final tabTitlesProvider = Provider<List<String>>((ref) {
   final user = ref.watch(userProvider);
@@ -8,5 +9,14 @@ final tabTitlesProvider = Provider<List<String>>((ref) {
     return [];
   }
 
-  return user.events.map((event) => event.eventId).toList();
+  bool isAllMembersPaid(event) => event.members.isNotEmpty &&
+          event.members.every((member) => member.status != PaymentStatus.unpaid);
+
+  final unpaidEvents = user.events.where((event) => !isAllMembersPaid(event)).toList();
+
+  final fullyPaidEvents = user.events.where((event) => isAllMembersPaid(event)).toList();
+
+  final sortedEvents = [...unpaidEvents, ...fullyPaidEvents];
+
+  return sortedEvents.map((event) => event.eventId).toList();
 });
