f5cca2a39084d188cc59e0bc5c8f148f09f51c52
oiM-o <sugar_sugar2000@icloud.com>
2025-07-31 19:03:25 +0900

    微修正

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -42,7 +42,14 @@ android {
         versionName = "1.0.0"
     }
 
-
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
 
     buildTypes {
         release {
