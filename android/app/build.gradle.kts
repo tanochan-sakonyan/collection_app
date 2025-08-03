3d4ba4e32f98d3b76b0c74c052614b95bad2f3fa
yumayuma708 <yumaskywalker708@gmail.com>
2025-08-03 19:48:14 +0900

    Androi用：ビルド番号を変更

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 25
         targetSdk = 35
-        versionCode = 11
+        versionCode = 12
         versionName = "first relrease"
     }
 
