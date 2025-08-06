05fd62598f11da00331a72af776c7d56bb8e2615
yumayuma708 <yumaskywalker708@gmail.com>
2025-08-06 16:00:47 +0900

    絶対パスに変更

--- lib/ui/components/line_member_delete_limit_countdown.dart
+++ lib/ui/components/line_member_delete_limit_countdown.dart
@@ -2,13 +2,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/svg.dart';
-import '../../data/model/freezed/event.dart';
-import '../../data/model/freezed/line_group.dart';
-import '../../generated/s.dart';
-import '../../provider/user_provider.dart';
+import 'package:mr_collection/data/model/freezed/event.dart';
+import 'package:mr_collection/data/model/freezed/line_group.dart';
+import 'package:mr_collection/generated/s.dart';
+import 'package:mr_collection/provider/user_provider.dart';
 import 'countdown_timer.dart';
 import 'dialog/line_group_update_countdown_dialog.dart';
-
 
 class LineMemberDeleteLimitCountdown extends ConsumerWidget {
   final Event currentEvent;
@@ -24,29 +23,25 @@ class LineMemberDeleteLimitCountdown extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return GestureDetector(
       onTap: () async {
-        if (DateTime.now().isAfter(currentEvent.lineMembersFetchedAt!.add(const Duration(hours: 24)))){
+        if (DateTime.now().isAfter(currentEvent.lineMembersFetchedAt!
+            .add(const Duration(hours: 24)))) {
           showDialog(
               context: context,
               builder: (ctx) => AlertDialog(
-                content: Text(
-                  S.of(context)!.cannotreflesh,
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context)
-                      .textTheme
-                      .labelSmall
-                      ?.copyWith(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w400,
-                      color: Colors.black),
-                ),
-              )
-          );
-        } else{
+                    content: Text(
+                      S.of(context)!.cannotreflesh,
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.labelSmall?.copyWith(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.black),
+                    ),
+                  ));
+        } else {
           final updatedGroup = await showDialog<LineGroup>(
             context: context,
             builder: (BuildContext context) {
-              return LineGroupUpdateCountdownDialog(
-                  currentEvent: currentEvent);
+              return LineGroupUpdateCountdownDialog(currentEvent: currentEvent);
             },
           );
           if (updatedGroup != null) {
@@ -61,39 +56,35 @@ class LineMemberDeleteLimitCountdown extends ConsumerWidget {
         children: [
           ...(currentEvent.lineMembersFetchedAt != null)
               ? [
-            Text(S.of(context)!.autoDeleteMemberCountdown,
-                style: Theme.of(context)
-                    .textTheme
-                    .labelSmall
-                    ?.copyWith(
-                    fontSize: 14, color: Colors.black)),
-            CountdownTimer(
-              expireTime: currentEvent.lineMembersFetchedAt!
-                  .add(const Duration(hours: 24)),
-              textStyle: Theme.of(context)
-                  .textTheme
-                  .labelSmall
-                  ?.copyWith(
-                fontSize: 14,
-                color: Colors.black,
-              ),
-              onExpired: () {
-                ref
-                    .read(userProvider.notifier)
-                    .clearMembersOfEvent(currentEventId);
-              },
-            ),
-            const SizedBox(width: 8),
-            SvgPicture.asset(
-              'assets/icons/ic_update.svg',
-              width: 32,
-              height: 32,
-            ),
-            const SizedBox(width: 36),
-          ]
+                  Text(S.of(context)!.autoDeleteMemberCountdown,
+                      style: Theme.of(context)
+                          .textTheme
+                          .labelSmall
+                          ?.copyWith(fontSize: 14, color: Colors.black)),
+                  CountdownTimer(
+                    expireTime: currentEvent.lineMembersFetchedAt!
+                        .add(const Duration(hours: 24)),
+                    textStyle: Theme.of(context).textTheme.labelSmall?.copyWith(
+                          fontSize: 14,
+                          color: Colors.black,
+                        ),
+                    onExpired: () {
+                      ref
+                          .read(userProvider.notifier)
+                          .clearMembersOfEvent(currentEventId);
+                    },
+                  ),
+                  const SizedBox(width: 8),
+                  SvgPicture.asset(
+                    'assets/icons/ic_update.svg',
+                    width: 32,
+                    height: 32,
+                  ),
+                  const SizedBox(width: 36),
+                ]
               : [
-            const SizedBox(height: 4),
-          ],
+                  const SizedBox(height: 4),
+                ],
         ],
       ),
     );
